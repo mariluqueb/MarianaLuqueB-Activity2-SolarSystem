31a992f311c17fd6e172c5bcfae22071f1cc611c
mariluqueb <mabelu_96@hotmail.com>
2018-08-25 16:50:00 -0500

    Hola

---
+++ Assets/Assets/Scripts/Translation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Translation : MonoBehaviour {
+
+    public Transform translationPivot;
+    public float translationSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.RotateAround(translationPivot.position, Vector3.up, translationSpeed * Time.deltaTime);	
+	}
+}
